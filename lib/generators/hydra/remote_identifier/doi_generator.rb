02c00f2d6c796f08b02d33ed9ee972837502ccc5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-08 15:39:50 -0400

    Adding configuration options for DOI generation

--- lib/generators/hydra/remote_identifier/doi_generator.rb
+++ lib/generators/hydra/remote_identifier/doi_generator.rb
@@ -1,11 +1,19 @@
 require 'rails/generators'
 
 class Hydra::RemoteIdentifier::DoiGenerator < Rails::Generators::Base
+  DEFAULT_CREDENTIALS_PATH = 'config/doi.yml'
+
   desc 'Register the target class names as acceptable for DOI'
-  class_options credentials_path: :string
+  class_option :credentials_path, type: :string, default: DEFAULT_CREDENTIALS_PATH, desc: 'Where can we find your DOI credentials'
+  class_option :target, type: :string, default: ":permanent_uri", desc: "What is the object's permanent_uri"
+  class_option :creator, type: :string, default: ":creator", desc: "Name of the object's creator"
+  class_option :title, type: :string, default: ":title", desc: "Title of the created object"
+  class_option :publisher, type: :string, default: ":publisher", desc: "Publisher of the created object"
+  class_option :publication_year, type: :string, default: ":publication_year", desc: "Year the created object was published"
+  class_option :set_identifier, type: :string, default: ":set_doi_identifier", desc: "Method on object to call when DOI is set"
   argument :targets, :type => :array, :default => [], :banner => "class_name class_name"
 
-  def create
+  def insert_doi
     inject_into_file(
       "config/initializers/hydra-remote_identifier_config.rb",
       after: /Hydra::RemoteIdentifier.*/
@@ -13,15 +21,15 @@ class Hydra::RemoteIdentifier::DoiGenerator < Rails::Generators::Base
 
       data = []
       data << ""
-      data << %(  doi_credentials = Psych.load("#{credentials_path}"))
+      data << %(  doi_credentials = Psych.load_file("#{credentials_path}"))
       data << %(  config.remote_service(:doi, doi_credentials) do |doi|)
       data << %(    doi.register(#{normalized_targets}) do |map|)
-      data << %(      map.target {|obj| obj.permanent_uri })
-      data << %(      map.creator :creator)
-      data << %(      map.title :title)
-      data << %(      map.publisher :publisher)
-      data << %(      map.publicationyear :publicationyear)
-      data << %(      map.set_identifier {|obj,value| obj.set_doi(value) })
+      data << %(      map.target #{options.fetch('target')})
+      data << %(      map.creator #{options.fetch('creator')})
+      data << %(      map.title #{options.fetch('title')})
+      data << %(      map.publisher #{options.fetch('publisher')})
+      data << %(      map.publicationyear #{options.fetch('publication_year')})
+      data << %(      map.set_identifier #{options.fetch('set_identifier')})
       data << %(    end)
       data << %(  end)
       data << ""
@@ -30,9 +38,19 @@ class Hydra::RemoteIdentifier::DoiGenerator < Rails::Generators::Base
     end
   end
 
+  def optionally_create_credentials_path
+    if credentials_path == DEFAULT_CREDENTIALS_PATH
+      require 'hydra/remote_identifier/remote_services/doi'
+      create_file(
+        credentials_path,
+        Hydra::RemoteIdentifier::RemoteServices::Doi::TEST_CONFIGURATION.to_yaml
+      )
+    end
+  end
+
   private
   def credentials_path
-    options.fetch('credentials_path', "./path/to/doi_credentials.yml")
+    options.fetch('credentials_path')
   end
 
   def normalized_targets
